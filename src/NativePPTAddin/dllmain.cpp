3d9e7fa41ca3e68914842eac67cda3ff1f28f1a9
l2m2 <l2m2lq@gmail.com>
2021-10-27 18:18:10 +0800

    fix: 修复在WPS上无法正常显示图标的问题

--- src/NativePPTAddin/dllmain.cpp
+++ src/NativePPTAddin/dllmain.cpp
@@ -46,6 +46,12 @@ namespace {
 // DLL 入口点
 extern "C" BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
 {
+    // 修复在WPS上无法正常显示图标的问题
+    // https://stackoverflow.com/questions/9163372/bitmapfromfile-method-returns-null-on-x86/9165501
+    Gdiplus::GdiplusStartupInput gdiplusStartupInput;
+    ULONG_PTR gdiplusToken;
+    Gdiplus::GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
+
     g_hInstance = hInstance;
     g_hWnd = GetMainWindow(::GetCurrentProcessId());
     return _AtlModule.DllMain(dwReason, lpReserved);
