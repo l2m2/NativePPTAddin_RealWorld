5965dee85e3ce8e78ebecb716d64709adbe6eae0
l2m2 <l2m2lq@gmail.com>
2021-10-27 11:21:24 +0800

    chore: 新增release模式下的依赖拷贝脚本

--- scripts/copy_deps_release.bat
+++ scripts/copy_deps_release.bat
@@ -1,3 +1,9 @@
 @echo off
 
-set DSTDIR=%~dp0
+set DST=%~dp0..\src\Release\
+set THIRDPARTY=%~dp0..\thirdparty\
+
+xcopy "%THIRDPARTY%duilib\bin\DuiLib_d.dll" "%DST%" /Y
+xcopy "%THIRDPARTY%duilib\bin\DuiLib_d.pdb" "%DST%" /Y
+
+xcopy %~dp0..\src\NativePPTAddin\resource\image "%DST%image\"
