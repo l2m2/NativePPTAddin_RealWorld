9047647e165ba8ac4ac702ebaa1c816e72a431a8
l2m2 <l2m2lq@gmail.com>
2021-10-27 17:22:28 +0800

    chore: 简化拷贝脚本

--- scripts/copy_deps_release.bat
+++ scripts/copy_deps_release.bat
@@ -1,9 +1,3 @@
 @echo off
 
-set DST=%~dp0..\src\Release\
-set THIRDPARTY=%~dp0..\thirdparty\
-
-xcopy "%THIRDPARTY%duilib\bin\DuiLib_d.dll" "%DST%" /Y
-xcopy "%THIRDPARTY%duilib\bin\DuiLib_d.pdb" "%DST%" /Y
-
-xcopy %~dp0..\src\NativePPTAddin\resource\image "%DST%image\"
+call %~dp0copy_deps_x.bat Release
---
+++ scripts/copy_deps_x.bat
@@ -0,0 +1,12 @@
+@echo off
+
+set Configuration=%1
+set DST=%~dp0..\src\%Configuration%\
+set THIRDPARTY=%~dp0..\thirdparty\
+
+xcopy "%THIRDPARTY%duilib\bin\DuiLib_d.dll" "%DST%" /Y
+xcopy "%THIRDPARTY%duilib\bin\DuiLib_d.pdb" "%DST%" /Y
+
+xcopy %~dp0..\src\NativePPTAddin\resource\image "%DST%image\" /Y
+
+xcopy "%THIRDPARTY%log4cpp\bin\%Configuration%\log4cpp.dll" "%DST%" /Y
